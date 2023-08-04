c502bfa889cfbb4da9e1b5bfea5930e7d6298a0a
zhouzhi96 <zhouzhi96@jd.com>
2023-08-04 16:24:42 +0800

    code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # myWebFlux
+
+ps aux | grep -v grep | grep mongod
+mongod --dbpath /usr/local/var/mongodb --logpath /usr/local/var/log/mongodb/mongo.log --fork
+--dbpath 设置数据存放目录
+--logpath 设置日志存放目录
+--fork 在后台运行
--- src/main/java/com/karl/myWebFlux/controller/UserController.java
+++ src/main/java/com/karl/myWebFlux/controller/UserController.java
@@ -3,10 +3,9 @@ package com.karl.myWebFlux.controller;
 import com.karl.myWebFlux.domain.User;
 import com.karl.myWebFlux.respository.UserRepository;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @Description: DESC
@@ -23,13 +22,40 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    /**
+     * @Description: 以数组形式一次返回
+     * @param:
+     * @return reactor.core.publisher.Flux<com.karl.myWebFlux.domain.User>
+     * @author zhouzhi96
+     * @date 2023/8/4 15:43
+     */
     @GetMapping("/all")
     public Flux<User> getAll(){
         return userRepository.findAll();
     }
 
+    /**
+     * @Description: SSE形式多次返回数据
+     * @param:
+     * @return reactor.core.publisher.Flux<com.karl.myWebFlux.domain.User>
+     * @author zhouzhi96
+     * @date 2023/8/4 15:44
+     */
     @GetMapping(value="/stream/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<User> streamGetAll(){
         return userRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public Mono<User> addUser(@RequestBody User user){
+        // spring data jpa 里面，新增和修改都是save，有id是修改，无id是新增
+        user.setId(null);
+        return this.userRepository.save(user);
+    }
+    @PostMapping("/update")
+    public Mono<User> updateUser(@RequestBody User user){
+        // spring data jpa 里面，新增和修改都是save，有id是修改，无id是新增
+//        user.setId(null);
+        return this.userRepository.save(user);
     }
 }
